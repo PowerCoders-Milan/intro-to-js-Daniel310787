b735c5cc9f54b309d441a16ff1b9102cd19967e8
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-02-25 11:30:02 +0100

    adding third exercise

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,13 @@
 // Example
-window.alert("Hello World!");
+
+var numOne = 25;
+var numSec = 25;
+
+if (numOne > numSec) {
+    console.log("The greater number of " + numOne + " and " + numSec + " is " + numOne + ".");
+} else if(numSec > numOne){
+    console.log("The greater number of " + numOne + " and " + numSec + " is " + numSec + ".")
+} else {
+    console.log("The Numbers are the same")
+}
+
