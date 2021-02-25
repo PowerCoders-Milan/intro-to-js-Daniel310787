ad54514bb74ad46b1f0706ddae187f34a0816cbd
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-02-25 11:09:31 +0100

    aqdding second exercise

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,2 +1,7 @@
 // Example
-window.alert("Hello World!");
+var birthday = prompt("Can I have your birth year", "0");  
+var futYear = prompt("Can I have the future year you want me to calculate", "0")
+
+var newAge = futYear - birthday;
+
+window.alert("You will be either " +newAge+ " or " +(newAge-1)+ " in " +futYear+ ".")
