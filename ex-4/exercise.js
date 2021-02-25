a55a0a02adebf14de6c2cddbcfdbe963a14f61d7
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-02-25 16:59:43 +0100

    finishing

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,12 +1,12 @@
 // Example
 var name = prompt("Can I have your Name");
 
-window.alert("Hello" +name+ "is nice to meet you");
+window.alert("Hello " +name+ " is nice to meet you");
 
 var yourDay = prompt("how your day is going?", "good or bad");
 
-if("good"){
+if(yourDay == "good"){
     window.alert("I'm happy for you champ")
-} else if ("bad") {
+} else if(yourDay == "bad") {
     window.alert("That's too bad, tomorrow it's a new day")
 }
